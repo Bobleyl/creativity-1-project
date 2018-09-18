f3a7b2cbf3a8af2ff11217a478d6d8626c09fe9b
EC2 Default User <ec2-user@ip-172-31-44-200.ec2.internal>
2018-09-18 04:13:41 +0000

    Attempt update fix

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Creativity Project #1
 
 Objective: Upon entering name into text field and clicking Submit, website generates customized realDonaldTrump tweet targeted at the user given name.
+
+<script type="text/javascript" src="tweet.js"></script>
--- tweet.js
+++ tweet.js
@@ -1,6 +1,26 @@
-  function format_tweet(event, context) {
+  function format_tweet(event) {
     var __name__ = document.getElementById("myform").value;
-    document.getElementById("demo").innerHTML = "Hello "+__name__;
-    location.href = "name.html";
+    var data = require('./data.json');
+    var array_len = data.length;
+    var index = parseInt((Math.random() * 20) % array_len);
+    var tweet = data[index]['tweet'].replace("__name__", __name__);
+    
+    window.location = "name.html";
+    document.getElementById("tweet").innerHTML = tweet;
     event.preventDefault();
   }
+  
+  /*
+  
+  <div id="yourOutputDiv"></div>
+
+var results = document.getElementById("activity_level").value + "</br>" +
+              document.getElementById("target_bodyfat_pct").value + 
+              document.getElementById("tw").value + "</br>";
+
+document.getElementById("yourOutputDiv").innerHTML = results;
+
+
+<button onclick="location.href = 'www.yoursite.com';" id="myButton" class="float-left submit-button" >Home</button>
+
+*/
